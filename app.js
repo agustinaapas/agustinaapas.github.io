fc122d11357be1892cb9597ecbe27a402ad27bee
agustinaapas <127274449+agustinaapas@users.noreply.github.com>
2023-03-08 00:16:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    alert("Una profesional con vistas al futuro, tratando de mantener los horizontes en nuevas experiencias, manteniendome atenta a las necesidades del mercado.");
+  }
+  function idiomasvarios() {
+    alert("Los idiomas son muy importante para mi ya que considero que te abre un mundo de posibilidades, donde podes conocer nuevas personas y sus respectivas culturas.");
+  }
